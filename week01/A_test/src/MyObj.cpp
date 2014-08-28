93bd7af8b654a2fe496417e1a57fa1452d2a00a3
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2014-08-28 07:10:13 -0400

    class examples

--- week01/A_test/src/MyObj.cpp
+++ week01/A_test/src/MyObj.cpp
@@ -7,3 +7,37 @@
 //
 
 #include "MyObj.h"
+
+void MyObj::setSrc(float _x, float _y){
+    src.set(_x,_y);
+}
+
+void MyObj::setDst(float _x, float _y){
+    dst.set(_x, _y);
+}
+
+void MyObj::updatePct(float _pct){
+//    pos.x = dst.x * _pct + src.x * (1.0-_pct);
+//    pos.y = dst.y * _pct + src.y * (1.0-_pct);
+
+    pos = dst * _pct + src *(1.0-_pct);
+}
+
+void MyObj::powUpdatePct(float _pct, float _shaper){
+    
+    float powPct = powf(_pct, _shaper);
+    pos = dst * powPct + src *(1.0-powPct);
+}
+
+void MyObj::draw(){
+    
+//    ofSetColor(255, 0, 0);
+    
+    ofSetColor(255,255,255,200);
+    ofLine(src, dst);
+    
+    ofSetColor(ofColor::red);
+    ofCircle(pos, 10);
+    
+    
+}
--- week01/A_test/src/MyObj.h
+++ week01/A_test/src/MyObj.h
@@ -1,14 +1,27 @@
 //
 //  MyObj.h
-//  00_test
 //
 //  Created by Patricio Gonzalez Vivo on 8/27/14.
 //
 //
 
-#ifndef ___0_test__MyObj__
-#define ___0_test__MyObj__
+#pragma once
 
-#include <iostream>
+#include "ofMain.h"
 
-#endif /* defined(___0_test__MyObj__) */
+class MyObj {
+public:
+    
+    void setSrc(float _x, float _y);
+    void setDst(float _x, float _y);
+    
+    void updatePct(float _pct); // _pct = [0.0 - 1.0]
+    void powUpdatePct(float _pct, float _shaper);
+    
+    void draw();
+    
+private:
+    ofPoint src, dst;
+    ofPoint pos;
+    
+};
