d3b2a20328d0bd548ba9432cb737a2c8b8dff4f1
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2014-11-05 21:47:47 -0500

    normals fixed + indexes + comments

--- week10/A_MeshAndMatrix/src/ofApp.cpp
+++ week10/A_MeshAndMatrix/src/ofApp.cpp
@@ -5,7 +5,7 @@ void ofApp::setup(){
     ofSetVerticalSync(true);
     ofEnableDepthTest();
     
-    mesh.setMode(OF_PRIMITIVE_TRIANGLE_STRIP);
+    mesh.setMode(OF_PRIMITIVE_TRIANGLES);
     
     light.setDiffuseColor(ofFloatColor(1.,1.,1.));
 }
@@ -24,6 +24,9 @@ void ofApp::update(){
     ofMatrix4x4 M;
     M.makeIdentityMatrix();
     
+    //  We put the vertexes with the colors
+    //  rotating their position using a matrix
+    //
     for (int i = 0 ; i < 100; i++) {
         ofMatrix4x4 R;
         R.makeRotationMatrix(10.0, ofVec3f(1,0,0));
@@ -42,40 +45,62 @@ void ofApp::update(){
         height += offset;
     }
     
+    //  We compute the normals
+    //
     mesh.clearNormals();
     ofPoint up = ofPoint(0,0,1);
     mesh.addNormal(up);
     mesh.addNormal(up);
     for (int i = 2; i < mesh.getVertices().size(); i+=2) {
-        ofPoint a = mesh.getVertices()[i-1];
-        ofPoint b = mesh.getVertices()[i];
-        ofPoint c = mesh.getVertices()[i+1];
-        ofVec3f normal = ((b - a).cross(c - a)).normalize();
+        ofPoint a1 = mesh.getVertices()[i-2];
+        ofPoint b1 = mesh.getVertices()[i-1];
+        ofPoint c1 = mesh.getVertices()[i];
+        ofVec3f normal1 = ((b1 - a1).cross(c1 - a1)).normalize();
+        mesh.addNormal(normal1);
         
-        mesh.addNormal(normal);
-        mesh.addNormal(normal);
+        ofPoint a2 = mesh.getVertices()[i-1];
+        ofPoint b2 = mesh.getVertices()[i];
+        ofPoint c2 = mesh.getVertices()[i+1];
+        ofVec3f normal2 = ((b2 - a2).cross(c2 - a2)).normalize();
+        mesh.addNormal(normal2);
     }
     
+    for (int i = 2; i < mesh.getVertices().size(); i+=2) {
+        mesh.addIndex(i-2);
+        mesh.addIndex(i-1);
+        mesh.addIndex(i);
+        
+        mesh.addIndex(i-1);
+        mesh.addIndex(i);
+        mesh.addIndex(i+1);
+    }
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofBackground(0);
+    
+    //  We move the light
+    //
     float t = ofGetElapsedTimef();
     light.setPosition(ofVec3f(cos(t*1.2)*400+200*-sin(t*0.5),
                               400*sin(t*0.5),
                               sin(t*1.6)*400)+200*-cos(t*0.5));
-}
-
-
-
-//--------------------------------------------------------------
-void ofApp::draw(){
-
-    ofBackground(0);
     
+    //  Camera on
+    //
     cam.begin();
     
     light.draw();
     
+    //  Lights on
+    //
     ofEnableLighting();
     light.enable();
 
+    //  Draw the mes
+    //
     mesh.draw();
     
     light.disable();
